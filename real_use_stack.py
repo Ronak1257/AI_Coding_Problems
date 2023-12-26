1cf0478e97d6a4f70e7c357b55e1e4ac6a67f4fe
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2023-12-26 19:31:37 +0530

    add real_use_case of stack algorithm

---
+++ real_use_stack.py
@@ -0,0 +1,39 @@
+# Implement the Stack Algorithm for any Real Time Use Case (no library use)
+
+# using stack we can implement copy paste in the computer this is best solution we can do
+
+class Real_Stack:
+    def __init__(self):
+        self.items=list()
+        self.top=-1
+
+    def is_empty(self):
+        return self.top==-1
+
+    def push(self,data):
+        self.items.append(data)
+        self.top+=1
+        print("Add in Clipboard : ",data)
+
+    def pop(self):
+        if not (self.is_empty()):
+            print("Remove from Clipboard : ",self.items[self.top])
+            self.top-=1
+    def paste(self):
+        if not (self.is_empty()):
+            print("Pasted data : ",self.items[self.top])
+
+Stack=Real_Stack()
+inp=4
+while(inp):
+    inp=int(input("Enter number : 1) push 2) pop 3) paste 0) exit : "))
+    match inp:
+        case 1:
+            da=input("Enter String : ")
+            Stack.push(da)
+        case 2:
+            Stack.pop()
+        case 3:
+            Stack.paste()
+            
+
